072b4742be1f3bb5a5836aabd72f87a4df4469f5
Albert Wong <atwong@alumni.uci.edu>
2020-07-27 14:53:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -88,10 +88,12 @@ app.get('/', function (req, res) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
-    col.aggregate( [
-      { $count: "myCount" }
-    ]);
-    res.render('index.html', { pageCountMessage : myCount, dbInfo: dbDetails });
+    col.countDocuments(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
   } else {
     res.render('index.html', { pageCountMessage : null});
   }
