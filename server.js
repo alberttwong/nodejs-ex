567ca13a206caab16680e826fdf6747f50f62794
Albert Wong <atwong@alumni.uci.edu>
2020-07-27 14:17:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
-    sleep(2000);
+    setTimeout(() => { console.log("Delay!"); }, 2000);
     col.count(function(err, count){
       if (err) {
         console.log('Error running count. Message:\n'+err);
